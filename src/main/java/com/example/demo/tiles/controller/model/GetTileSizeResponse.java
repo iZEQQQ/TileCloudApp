8119b4f8eb5e053753d465b994a10a55e4309ef8
jgorny <iceziro@wp.pl>
2021-01-25 20:16:20 +0100

    fix: problem with login feat: added pagination

---
+++ src/main/java/com/example/demo/tiles/controller/model/GetTileSizeResponse.java
@@ -0,0 +1,14 @@
+package com.example.demo.tiles.controller.model;
+
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@EqualsAndHashCode
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+public class GetTileSizeResponse {
+    Long size;
+}
