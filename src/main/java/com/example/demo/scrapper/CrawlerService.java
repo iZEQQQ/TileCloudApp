97c3fef0812d1322b5e118e8adf4eb067873ba8c
jgorny <iceziro@wp.pl>
2021-01-30 21:17:51 +0100

    feat: unlocked project

--- src/main/java/com/example/demo/scrapper/CrawlerService.java
+++ src/main/java/com/example/demo/scrapper/CrawlerService.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.PostConstruct;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
@@ -24,7 +25,7 @@ import java.util.regex.Pattern;
  * Its a two simple crowler service for providing data from two websites:
  * - www.eplytki.pl
  * - www.domus-sklep.pl
- *
+ * <p>
  * crowlers use jsoup to get the connection afterwards we get data from elements.
  * After that method changes photos to bytes so the sorage is easy and efficient.
  * Then using vision from google we get read the photo and type is automaticly assigned
@@ -36,35 +37,27 @@ import java.util.regex.Pattern;
 @Service
 public class CrawlerService {
 
-    private TileService service;
+    private final TileService service;
 
     @Autowired
     public CrawlerService(TileService service) {
         this.service = service;
     }
 
-
-
-//    TODO dodac po rejestracji mozliwosc oceniania
-//    TODO dodac lokalizajce z ktorego sklepu zebrano
-
-//    @PostConstruct
-//    public void init() throws IOException {
-//        scrapEplytki();
-//        scrapDomus();
-//    }
+    @PostConstruct
+    public void init() throws IOException {
+        scrapEplytki();
+        scrapDomus();
+    }
 
 
     @Scheduled(cron = "0 0 * * 0")
     public void scrapEplytki() throws IOException {
         String url = "https://www.eplytki.pl/plytki-lazienkowe.html?limit=72";
-        //TODO przestawic na 4 strony finalnie
-        for (int i = 0; i < 1; i++) {
+        for (int i = 0; i < 4; i++) {
             Document doc = Jsoup.connect(url).get();
             Elements items = doc.select(".flag-p.item.last");
-            items.stream().limit(4).forEach(element -> {
-//                TODO odblokawac do wszystkich plytek z strony i usunanc ta wyzej
-//            items.forEach(element -> {
+            items.forEach(element -> {
                 Elements a = element.select(".product-image");
                 String title = a.attr("title");
                 Elements img = a.select(".lazyload");
@@ -136,13 +129,10 @@ public class CrawlerService {
     @Scheduled(cron = "0 0 * * 0")
     public void scrapDomus() throws IOException {
         String url = "https://domus-sklep.pl/1107-wszystkie-plytki";
-//        TODO przestawic na 4 strony finalnie
-        for (int i = 0; i < 1; i++) {
-                    Document doc = Jsoup.connect(url).get();
+        for (int i = 0; i < 4; i++) {
+            Document doc = Jsoup.connect(url).get();
             Elements items = doc.select(".pro_outer_box");
-//            items.forEach(element -> {
-//            TODO finalnie odblokowac by nie pobieralo tylko 4 itemow a wszystkie z strony
-            items.stream().limit(4).forEach(element -> {
+            items.forEach(element -> {
                 Elements img = element.select(".replace-2x.img-responsive.menu_pro_img");
                 Elements imgLazy = element.select(".lazy-loading.replace-2x.img-responsive.front-image");
                 String imgUrlLazy = imgLazy.attr("data-src");
@@ -169,7 +159,7 @@ public class CrawlerService {
                     } catch (IOException e) {
                         e.printStackTrace();
                     }
-                    if(imgBytes != null){
+                    if (imgBytes != null) {
                         try (PredictionServiceClient client = PredictionServiceClient.create()) {
                             ModelName name = ModelName.of("217603025665", "us-central1", "ICN616089350391726080");
                             ByteString content = ByteString.copyFrom(imgBytes);
@@ -193,7 +183,7 @@ public class CrawlerService {
                         }
                     }
                     Tile tile = Tile.builder()
-                            .name(title+titleLazy)
+                            .name(title + titleLazy)
                             .photo(imgBytes)
                             .type(type)
                             .page("domus")
